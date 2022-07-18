84ad31ffe193532d50e7bdde1a392933b60276a9
ahirwarankit63 <ahirwarankit63@gmail.com>
2022-07-18 13:09:48 +0530

    databse connection

---
+++ backend/models/productModel.js
@@ -0,0 +1,81 @@
+const mongoose = reuire("mongoose");
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter product name"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter product Description"],
+  },
+
+  price: {
+    type: Number,
+    required: [true, "Please enter product price"],
+    maxLenght: [8, "pi\rice cannot exceed 8 character"],
+  },
+
+  rating: {
+    type: Number,
+    default: 0,
+  },
+
+  images: [
+    {
+      publid_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+
+  category: {
+    type: String,
+    required: [true, "Please enter the product category"],
+  },
+
+  Stock: {
+    type: Number,
+    required: [true, " please enter te product stock"],
+    maxLength: [4, "Stock cannot exceed 4 characters"],
+    default: 1,
+  },
+
+  numOfReviews : {
+    type : Number,
+    default : 0
+  },
+
+  reviews : [ {
+    name : { 
+        type : String,
+        required : true,
+
+    },
+
+    rating : {
+        type : Number,
+        required : true
+    },
+
+    comment : {
+        type : String,
+        required : true
+    }
+  }],
+
+  createdAt : {
+    type : Date,
+    default : Date.now
+  }
+
+
+
+});
+
+module.exports = mongoose.model("Product", productSchema)
