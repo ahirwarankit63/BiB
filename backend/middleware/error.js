7a8153e88993cc22b7a27455faa0e6a7fd7a9881
ahirwarankit63 <ahirwarankit63@gmail.com>
2022-08-04 15:00:41 +0530

    Mongo wrong id Error handling

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,8 +1,14 @@
-const ErrorHander = require("../utils/errorhander");
+const ErrorHandler = require("../utils/errorhander");
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "internal server error";
+
+// Wrong MongoDb Id Error
+if(err.name === "CastError") {
+  const message = `Resource not found. Invalid Id: ${err.path}`;
+  err = new ErrorHandler(message, 400);
+}
 
   res.status(err.statusCode).json({
     success: false,
