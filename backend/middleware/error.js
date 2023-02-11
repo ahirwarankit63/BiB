c57ee093b3fac59488f493d940875bc40340c3aa
ahirwarankit63 <ahirwarankit63@gmail.com>
2023-02-11 11:35:14 +0530

    error heandling for dupliate email

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,6 +10,12 @@ if(err.name === "CastError") {
   err = new ErrorHandler(message, 400);
 }
 
+// Mongoose duplicate key error
+if(err.code === 11000){
+  const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+  err = new ErrorHandler(message, 400)
+}
+
   res.status(err.statusCode).json({
     success: false,
     // error: err,
