23f16d19aceb20bc1c38a82e2d4f4b5ff1629fe3
ahirwarankit63 <ahirwarankit63@gmail.com>
2023-04-01 18:40:56 +0530

    updated loader

--- frontend/src/component/Layout/Loader/loader.js
+++ frontend/src/component/Layout/Loader/loader.js
@@ -2,14 +2,16 @@ import React from 'react'
 import "./loader.css";
 const loader = () => {
   return (
-    <div class="loader-container">
-    <div class="loader">
-      <div class="circle"></div>
-      <div class="triangle"></div>
-    </div>
-  </div>
-  
-  
+<div class="loader">
+  <div class="ring"></div>
+  <div class="ring"></div>
+  <div class="ring"></div>
+  <div class="ring"></div>
+  <div class="ring"></div>
+  <div class="ring"></div>
+  <div class="ring"></div>
+  <div class="ring"></div>
+</div>
 
   )
 }
